c54e8ad35eb20121c620bc9c47163fb5abfcca20
Aman <amangarg0303@gmail.com>
2023-12-23 13:35:35 +0530

    added prisma user schema

--- src/components/AuthNav.tsx
+++ src/components/AuthNav.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function AuthNav() {
   return (
     <div className="flex gap-1 absolute top-2 left-2 lg:top-5 lg:left-10 items-center">
       <Image src="/images/logo.png" width="50" height="50" alt="logo" />
-      <h1 className="text-2xl font-bold">DevUi</h1>
+      <Link href="/">
+        <h1 className="text-2xl font-bold">DevUi</h1>
+      </Link>
     </div>
   );
 }
